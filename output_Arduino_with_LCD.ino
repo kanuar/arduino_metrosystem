84f63fe2367fe2a523d0b037b41b3d2ba1fefb61
kanuar <68312266+kanuar@users.noreply.github.com>
2022-09-07 16:14:23 +0530

    Update output_Arduino_with_LCD.ino

--- output_Arduino_with_LCD.ino
+++ output_Arduino_with_LCD.ino
@@ -1,59 +1,48 @@
-// C++ code
-//
-int entrypd = 0;
+#include <LiquidCrystal.h>
 
-int exitpd = 0;
-
-int entrycd = 0;
-
-int exitcd = 0;
-
-int counter = 0;
-
-int temp = 0;
-
-long readUltrasonicDistance(int triggerPin, int echoPin)
-{
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(echoPin, HIGH);
+#include<Wire.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int counter=0;
+void setup() {
+  lcd.begin(16,2);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Wire.begin(8);             
+  Wire.onReceive(receiveEvent); 
+  Serial.begin(9600);
+  lcd.setCursor(0,0);
+  lcd.print("Current Count : ");
 }
 
-void setup()
-{
-  pinMode(A0, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(7, OUTPUT);
+void loop() {
+  delay(10);
 }
-
-void loop()
+void receiveEvent(int howMany) 
 {
-  entrypd = 0;
-  exitpd = 0;
-  counter = 0;
-  temp = 0;
-
-  if (temp != (-40 + 0.488155 * (analogRead(A0) - 20))) {
-    temp = (-40 + 0.488155 * (analogRead(A0) - 20));
-    if ((-40 + 0.488155 * (analogRead(A4) - 20)) > 39) {
-      tone(7, 440, 1000); // play tone 57 (A4 = 440 Hz)
-      noTone(7);
-    } else {
-      if (0.01723 * readUltrasonicDistance(9, 10) < 30) {
-        counter += 1;
-        delay(5000); // Wait for 5000 millisecond(s)
-      }
-    }
+  while (1 < Wire.available()) 
+  { 
+    char c = Wire.read();    
   }
-  if (0.01723 * readUltrasonicDistance(2, 4) < 30) {
-    counter = (counter - 1);
-    delay(5000); // Wait for 5000 millisecond(s)
+  int x=Wire.read(); 
+  if (x==102)
+  {
+    counter+=1;
+    
+    lcd.setCursor(0,1);
+    lcd.print(counter);
+    delay(5000);
+  }
+  if (x==101)
+  {
+    counter-=1;
+    lcd.setCursor(0,1);
+    lcd.print(counter);
+    delay(5000);
+  }
+  if (counter<0)
+  {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Current Count : ");
+    
   }
 }
